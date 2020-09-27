2722e1eb02aa1e34a3c6fb10c67b4ae8119db4ed
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-27 16:27:39 -0600

    restructured files, added user prompts

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-const inquirer = require('inquirer');
+const inquirer = require("inquirer");
+
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: "What is the title of the Project (Note, this will be the title of your README)?"
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project.'
+        },
+        {
+            type: 'confirm',
+            name: 'contentsTable',
+            message: 'Would you like a table of contents?'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please provide a step-by-step description of how to get the development environment running.'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please provide instructions and examples for how to use your application.'
+        },
+        {
+            type: '',
+            name: 'license',
+            message: 'Please select any and all Licenses that apply. A badge will be added for each.'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'How can other developers contribute to this project? Please provide guidelines on how to do so.'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Please provide a test for your application, and provide an example on how to run it.'
+        }
+    ]);
+};
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
