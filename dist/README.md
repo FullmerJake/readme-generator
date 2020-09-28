00f3f826a64efb2f7ff2c0d18e027796cd9d0c1d
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-09-27 18:47:50 -0600

    reformatted table of contents

---
+++ dist/README.md
@@ -0,0 +1,33 @@
+
+  # asdf
+  ## Description
+  asdgasd
+
+  ## Table of Contents
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+    *[Contributing](#contributing)
+    *[Tests](#tests)
+    *[Questions](#questions)
+
+  ## Installation
+  fasdgasdg
+
+  ## Usage
+  asdfasdg
+
+  ## License
+  Javascript,HTML
+
+  ## Contributing
+  asdgasdf
+
+  ## Tests
+  adsgasdfasdf
+  
+  ## Questions
+  asdgasdf
+  asgd
+
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,18 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
 
-
   return `
   # ${data.title}
   ## Description
   ${data.description}
 
   ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
+    *[Installation](#installation)
+    *[Usage](#usage)
+    *[License](#license)
+    *[Contributing](#contributing)
+    *[Tests](#tests)
+    *[Questions](#questions)
 
   ## Installation
   ${data.installation}
